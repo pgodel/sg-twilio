3af3b3429f46435d9670109a7ca181078bf0930f
Pablo Godel <pgodel@gmail.com>
2013-07-13 17:33:31 -0400

    working app

--- src/app.php
+++ src/app.php
@@ -1,48 +1,179 @@
 <?php
 
-$app['cfg'] = $app->share(function()
-{
-    return \Symfony\Component\Yaml\Yaml::parse(__DIR__.'/../config/config.yml');
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+$app['cfg'] = $app->share(function () {
+    return \Symfony\Component\Yaml\Yaml::parse(__DIR__ . '/../config/config.yml');
 });
 
-$app->get('/call/{number}', function ($number) use ($app) {
-
+$app['api'] = function ($app) {
     $api = new \ServerGrove\APIClient('https://control.servergrove.com');
     $api->setApiKey($app['cfg']['sg_api']['key']);
     $api->setApiSecret($app['cfg']['sg_api']['secret']);
 
+    return $api;
+};
+
+$app->post('/server/{number}/{serverId}', function ($number, $serverId, Request $request) use ($app) {
+
+    $api = $app['api'];
+
+    $digits = $request->request->get('Digits');
+
+    $response = new Services_Twilio_Twiml();
+
+    switch ($digits) {
+        case 1:
+            $api->call('server/stop', array('debug' => 1, 'serverId' => $serverId));
+            $response->say("Your server is being stopped.");
+            break;
+        case 2:
+            $api->call('server/start', array('debug' => 1, 'serverId' => $serverId));
+            $response->say("Your server is being started.");
+            break;
+        case 3:
+            $api->call('server/restart', array('debug' => 1, 'serverId' => $serverId));
+            $response->say("Your server is being restarted.");
+            break;
+        case 4:
+            $result = $api->call('domain/list', array('serverId' => $serverId));
+            $apirsp = $api->getResponse('array');
+
+            $message = 'Server has the following domains: ';
+            foreach ($apirsp['rsp'] as $dom) {
+                $message .= ', ' . $dom['name'];
+            }
+
+            $response->say($message);
+            break;
+        default:
+            $response->say("Your selection is invalid.");
+            break;
+    }
+
+    $response->redirect('http://' . $request->getHttpHost() . '/server/' . $number . '/' . $serverId,
+        array('method' => 'GET')
+    );
+
+    $rsp = new Response($response, 200, array(
+        'content-type' => 'application/xml'
+    ));
+    return $rsp;
+});
+
+
+$app->get('/server/{number}/{serverId}', function ($number, $serverId) use ($app) {
+
+    $api = $app['api'];
+
+    $result = $api->call('server/get', array('serverId' => $serverId));
+    $server = $api->getResponse('array');
+    $s = $server['rsp'];
+    $response = new Services_Twilio_Twiml();
+
+    $gather = $response->gather(array(
+        'numDigits' => 1,
+        'timeout' => 30,
+    ));
+
+    $gather->say('You have selected server ' . $s['hostname'] . ' and the status is running. Select 1 to stop, 2 to start, 3 to restart, 4 to list the domains in the server');
+    $rsp = new Response($response, 200, array(
+        'content-type' => 'application/xml'
+    ));
+    return $rsp;
+});
+
+$app->post('/servers/{number}', function ($number, Request $request) use ($app) {
+    $host = $request->getHttpHost();
+
+    $api = $app['api'];
+
+    $result = $api->call('server/list');
+    $apirsp = $api->getResponse('array');
+
+    $digits = $request->request->get('Digits');
+    $response = new Services_Twilio_Twiml();
+
+
+    if (!empty($digits)) {
+        $server = null;
+        $i = 1;
+        foreach ($apirsp['rsp'] as $s) {
+            if ($i == $digits) {
+                $server = $s;
+                break;
+            }
+            $i++;
+        }
+        if (!$server) {
+            $response->say("Your selection is invalid.");
+            $response->redirect('http://' . $host . '/servers/' . $number,
+                array('method' => 'GET')
+            );
+        } else {
+            $response->redirect('http://' . $host . '/server/' . $number . '/' . $server['id'],
+                array('method' => 'GET')
+            );
+        }
+    } else {
+        $response->redirect('http://' . $host . '/servers/' . $number,
+            array('method' => 'GET')
+        );
+    }
+
+    $rsp = new Response($response, 200, array(
+        'content-type' => 'application/xml'
+    ));
+    return $rsp;
+
+});
+
+$app->get('/servers/{number}', function ($number, Request $request) use ($app) {
+
+    $host = $request->getHttpHost();
+
+    $api = $app['api'];
+
     $result = $api->call('server/list');
     $servers = $api->getResponse('array');
 
+    $response = new Services_Twilio_Twiml();
 
-    $greeting = 'Howdy! Select a server from the following list: ';
+    if (empty($number)) {
+        $number = $request->query->get('From');
+    }
+
+    $gather = $response->gather(array(
+        'numDigits' => 1,
+        'timeout' => 30,
+        'action' => 'http://' . $host . '/servers/' . $number,
+    ));
+    $greeting = 'Howdy! Select a server from the following list:';
 
     $i = 1;
-    $options = array();
-    foreach($servers['rsp'] as $s) {
-        $greeting .= ", Press $i for ".$s['hostname'];
-        $options[$i] = 'http://twimlets.com/message?'.urlencode('Message[0]').'='.urlencode('You have selected server '.$s['hostname']);
+    foreach ($servers['rsp'] as $s) {
+        $greeting .= ", Press $i for " . $s['hostname'];
         $i++;
     }
 
-    $url = 'http://twimlets.com/menu?Message='.urlencode($greeting);
+    $gather->say($greeting);
+    $rsp = new Response($response, 200, array(
+        'content-type' => 'application/xml'
+    ));
+    return $rsp;
+})->value('number', null);
 
-    foreach($options as $i => $o) {
-        $url .= '&'.urlencode('Options['.$i.']').'='.urlencode($o);
-    }
 
+$app->get('/call/{number}', function ($number, Request $request) use ($app) {
     $client = new Services_Twilio($app['cfg']['twilio']['sid'], $app['cfg']['twilio']['token']);
-    //return print_r($url, true);
+
     $call = $client->account->calls->create(
         $app['cfg']['twilio']['number'], // From a valid Twilio number
-      $number, // Call this number
-
-      // Read TwiML at this URL when a call connects (hold music)
-    //'http://twimlets.com/menu?Message=Hello!%20Press%201%20for%20help%2C%20press%202%20for%20more%20help&Options%5B1%5D=http%3A%2F%2Fservergrove.com%2F1&Options%5B2%5D=http%3A%2F%2Fservergrove.com%2F2&'
-    // 'http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient'
-    $url
+        $number, // Call this number
+            'http://' . $request->getHttpHost() . '/servers/' . $number
     );
 
-    return $url;
+    return 'Initiating call to ' . $number;
 });
 
